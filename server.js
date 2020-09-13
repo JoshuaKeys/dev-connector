5c0eff57a80661f928e571d32655a303e87eb8c3
Joshua <joshua.oguma@outlook.com>
2020-09-13 19:31:32 +0300

    made some fixes

--- server.js
+++ server.js
@@ -21,8 +21,7 @@ app.use('/api/auth', require('./routes/api/auth'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
-
+  app.use(express.static('client-react/build'));
   app.get('*', (req, res) => {
     res.sendFile(
       path.resolve(__dirname, 'client-react', 'build', 'index.html')
