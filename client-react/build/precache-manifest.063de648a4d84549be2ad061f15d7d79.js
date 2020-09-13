566c5d504dbe54e2f92c2de2a11b8dd921a005d8
Joshua <joshua.oguma@outlook.com>
2020-09-13 19:24:26 +0300

    adding to git

---
+++ client-react/build/precache-manifest.063de648a4d84549be2ad061f15d7d79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a62b389e1153c6ae3e3145933a519e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "729b92cf9224926588e8",
+    "url": "/static/css/main.835ba491.chunk.css"
+  },
+  {
+    "revision": "f9f35e3989283e0abddb",
+    "url": "/static/js/2.53119f64.chunk.js"
+  },
+  {
+    "revision": "e293c143e177a32373554d6ca4c08bba",
+    "url": "/static/js/2.53119f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "729b92cf9224926588e8",
+    "url": "/static/js/main.e2c3bfb0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
